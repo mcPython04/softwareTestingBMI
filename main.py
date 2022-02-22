e6f5bfeb62d3152179eac328eb62f428182e6933
mcPython04 <chen1911893611@gmail.com>
2022-02-22 10:22:34 -0600

    Code on BMI Calculation

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+def main():
+    print("Please enter the required information")
+    height_feet = int(input("Please enter your height (ft.): "))
+    height_inch = int(input("Please enter your height (in.): "))
+    weight = int(input("Please enter your weight (lbs.): "))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    # Calculate BMI based on values inputted
+    bmi = round(calculate_bmi(height_feet, height_inch, weight), 2)
+    print("BMI: ", bmi)
+
+    # Outputs message based on BMI value
+    if bmi < 18.5:
+        print("Underweight")
+
+    elif bmi >= 18.5 or bmi < 25:
+        print("Normal Weight")
+
+    elif bmi >= 25 or bmi < 30:
+        print("Overweight")
+
+    elif bmi >= 30:
+        print("Obese")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Returns BMI value
+def calculate_bmi(feet, inch, weight):
+
+    # Step 1
+    kg = weight * 0.45
+
+    # Returns height in inches
+    height_inch = calculate_height(feet, inch)
+
+    # Step 2
+    height_meter = height_inch * 0.025
+
+    # Step 3
+    height_meter *= height_meter
+
+    # Step 4
+    result = kg/height_meter
+
+    return result
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Calculates height in inches
+def calculate_height(feet, inch):
+    flag = feet * 12
+    flag += inch
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return flag
+
+
+if __name__ == "__main__":
+    main()
+
+
+
